268c56f09d20657d2a7dcd3c209cdd5b16a319e9
DoroYoshi <yoshimatsu44dy@gmail.com>
2021-08-01 06:32:31 +0900

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy ]
+
   def index
     @posts = Post.all
+    # 最新記事用
+　   @new_posts = Post.all
   end
 
   def new
@@ -17,15 +21,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: "記事を更新しました"
     else
@@ -34,7 +37,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "記事を削除しました"
   end
@@ -43,5 +45,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
