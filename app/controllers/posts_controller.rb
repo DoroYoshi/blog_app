675cda93cdf1427e93371b250eedab7961a0b100
DoroYoshi <yoshimatsu44dy@gmail.com>
2021-08-01 06:37:21 +0900

    記事の5件表示をコントローラ側で処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all
-    # 最新記事用
-　   @new_posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+　   @new_posts = Post.all.order(created_at: :desc).limit(5)
   end
 
   def new
