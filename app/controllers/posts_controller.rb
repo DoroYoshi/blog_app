b1bed433c99658f2a4232c2d5ce7ce3fbbedece2
DoroYoshi <yoshimatsu44dy@gmail.com>
2021-08-01 15:39:09 +0900

    Postモデルにuser_idを追加し、アソシエーションを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,9 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.attributes = {
+      user_id: current_user.id
+    }
     if @post.save
       redirect_to @post, notice: "記事を投稿しました。"
     else
